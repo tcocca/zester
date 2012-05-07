f6ad3a7264dee8efbf1af6c187a3e37283f8da81
Tom Cocca <tom.cocca@gmail.com>
2012-05-07 10:19:13 -0400

    added property details api calls

--- lib/zester/property.rb
+++ lib/zester/property.rb
@@ -1,5 +1,27 @@
 module Zester
   class Property < Resource
 
+    def deep_comps(params = {})
+      if params['zpid'].nil?
+        raise ArgumentError, "zpid is required"
+      end
+      params['count'] ||= 10
+      get_results('GetDeepComps', :comps, params)
+    end
+
+    def deep_search_results(params = {})
+      if params['address'].nil? || params['citystatezip'].nil?
+        raise ArgumentError, "address and citystatezip are required"
+      end
+      get_results('GetDeepSearchResults', :searchresults, params)
+    end
+
+    def updated_property_details(params = {})
+      if params['zpid'].nil?
+        raise ArgumentError, "zpid is required"
+      end
+      get_results('GetUpdatedPropertyDetails', :updated_property_details, params)
+    end
+
   end
 end
--- spec/zester/property_spec.rb
+++ spec/zester/property_spec.rb
@@ -2,4 +2,101 @@ require 'spec_helper'
 
 describe Zester::Property do
   let!(:zester) {new_zester}
+  let!(:property) {zester.property}
+
+  context "deep_comps" do
+    it "should check for required params" do
+      VCR.use_cassette('property') do
+        expect {property.deep_comps}.to raise_error(ArgumentError, "zpid is required")
+        expect {property.deep_comps('zpid' => '12345')}.to_not raise_error(ArgumentError)
+      end
+    end
+
+    it "should return a success" do
+      VCR.use_cassette('property') do
+        response = property.deep_comps('zpid' => '48749425')
+        response.success?.should be_true
+        response.properties.should_not be_nil
+      end
+    end
+
+    it "should return a failure" do
+      VCR.use_cassette('property') do
+        response = property.deep_comps('zpid' => 'xyz')
+        response.success?.should be_false
+        response.response_code.should ==  500
+        response.error_message.should_not be_nil
+        response.should_not respond_to(:properties)
+      end
+    end
+  end
+
+  context "deep_search_results" do
+    it "should check for required params" do
+      VCR.use_cassette('property') do
+        expect {property.deep_search_results}.to raise_error(ArgumentError, "address and citystatezip are required")
+        expect {property.deep_search_results('address' => '2114 Bigelow Ave', 'citystatezip' => 'Seattle, WA')}.to_not raise_error(ArgumentError)
+      end
+    end
+
+    it "should return a success" do
+      VCR.use_cassette('property') do
+        response = property.deep_search_results('address' => '2114 Bigelow Ave', 'citystatezip' => 'Seattle, WA')
+        response.success?.should be_true
+        response.results.should_not be_nil
+      end
+    end
+
+    it "should return a failure" do
+      VCR.use_cassette('property') do
+        response = property.deep_search_results('address' => '2114 Bigelow Ave', 'citystatezip' => '123')
+        response.success?.should be_false
+        response.response_code.should ==  508
+        response.error_message.should_not be_nil
+        response.should_not respond_to(:results)
+      end
+    end
+  end
+
+  context "updated_property_details" do
+    it "should check for required params" do
+      VCR.use_cassette('property') do
+        expect {property.updated_property_details}.to raise_error(ArgumentError, "zpid is required")
+        expect {property.updated_property_details('zpid' => '12345')}.to_not raise_error(ArgumentError)
+      end
+    end
+
+    it "should return a success" do
+      VCR.use_cassette('property') do
+        response = property.updated_property_details('zpid' => '48749425')
+        response.success?.should be_true
+        response.zpid.should_not be_nil
+        response.page_view_count.should_not be_nil
+        response.address.should_not be_nil
+        response.links.should_not be_nil
+        response.edited_facts.should_not be_nil
+        response.neighborhood.should_not be_nil
+        response.school_district.should_not be_nil
+        response.elementary_school.should_not be_nil
+        response.middle_school.should_not be_nil
+      end
+    end
+
+    it "should return a failure" do
+      VCR.use_cassette('property') do
+        response = property.updated_property_details('zpid' => 'xyz')
+        response.success?.should be_false
+        response.response_code.should ==  500
+        response.error_message.should_not be_nil
+        response.should_not respond_to(:address)
+        response.should_not respond_to(:links)
+        response.should_not respond_to(:edited_facts)
+        response.should_not respond_to(:neighborhood)
+        response.should_not respond_to(:school_district)
+        response.should_not respond_to(:elementary_school)
+        response.should_not respond_to(:middle_school)
+      end
+    end
+  end
+
 end

494dffc7b206963ce11766643d9f1e960e5b1213
Sergiy Kukunin <sergey.kukunin@gmail.com>
2019-07-27 15:23:01 +0300

    Don't swallow exceptions
    While it's arguably for rescuing timeouts, it's wrong to swallow all
    exceptions.
    
    For example, I spent about an hour figuring out why webmock doesn't work
    for zester, while it worked everywhere else. By default, webmock disables
    all http requests, and raises exceptions with suggestion to mock it. But
    in case with zester, it returned just `Web services are currently
    unavailable`, which was really weird: webmock seemed not working and API
    call was made, but Zillow returned an error about unavailability.
    After all I had to debug to this method.
    
    If exceptions were not swallowed, it was clear where the problem is.

--- spec/zester/resource_spec.rb
+++ spec/zester/resource_spec.rb
@@ -26,13 +26,9 @@ describe Zester::Resource do
       VCR.turn_on!
     end
 
-    it "should rescue a timeout error" do
+    it "should not rescue a timeout error" do
       stub_request(:get, "http://www.zillow.com/webservice/GetRateSummary.htm?zws-id=#{ZWS_ID}").to_timeout
-      response = resource.get_results('GetRateSummary', :rate_summary)
-      response.success?.should be_false
-      response.message.should_not be_nil
-      response.message.code.should == "3"
-      response.response_code.should == 3
+      expect {get_response}.to raise_error(Timeout::Error)
     end
   end
 
